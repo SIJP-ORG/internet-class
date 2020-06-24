df101da926bbd00a50dd11eccc35963c5f2e8a0b
Hiroshi Ohno <46858959+hiroshi-ohno@users.noreply.github.com>
2020-06-23 23:26:31 -0700

    messageapp initial

--- message/messageapp/main.py
+++ message/messageapp/main.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask, redirect, url_for
+from flask.json import JSONEncoder
+from datetime import date
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(
@@ -14,6 +16,29 @@ try:
 except OSError:
     pass
 
+# database init
+from . import db
+db.init_app(app)
+
+
+# Import other files
+from . import msg
+app.add_url_rule('/msg/sendnew', view_func=msg.sendnew, methods=['POST'])
+app.add_url_rule('/msg', view_func=msg.getall, methods=['GET'])
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        try:
+            if isinstance(obj, date):
+                return obj.isoformat()
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return JSONEncoder.default(self, obj)
+
+app.json_encoder = CustomJSONEncoder
 
 # Root
 @app.route('/')
@@ -25,7 +50,15 @@ def root():
 def hello():
     return 'Hello. I am alive.'
 
+# Database init
+@app.route('/init-db')
+def init_db():
+    """Clear the existing data and create new tables."""
+    db.init_db()
+    return 'Initialized the database.'
+
 # For debugging server. From the parent folder,
 # python -m messageapp.main
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
+
---
+++ message/messageapp/msg.py
@@ -0,0 +1,33 @@
+import json
+from flask import Flask, request, jsonify
+import sqlite3
+import datetime
+from . import db
+
+def test():
+    return 'it works'
+
+def sendnew():
+    '''API to send a new message by the caller'''
+    data = json.loads(request.data)
+
+    con = db.get_db()
+    cur = con.cursor()
+    cur.execute(
+        'insert into msg (sender, arrival, body) values (?, ?, ?)',
+        ('dummy-sender', datetime.datetime.now(), data['msg'])
+    )
+    rowid = cur.lastrowid
+    con.commit()
+
+    response = {'id': rowid, 'echo': data['msg']}
+    return jsonify(response)
+
+def getall():
+    '''API to return all messages'''
+    msgs = db.get_db().execute(
+        'select id, sender, arrival, body'
+        ' from msg'
+    ).fetchall()
+
+    return jsonify(msgs)
