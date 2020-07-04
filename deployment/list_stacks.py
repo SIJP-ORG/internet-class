7fa58587d8ccdd857b72989819eee1bf3c713bd1
Hiroshi Ohno <46858959+hiroshi-ohno@users.noreply.github.com>
2020-07-03 21:01:03 -0700

    Update stack list

--- deployment/list_stacks.py
+++ deployment/list_stacks.py
@@ -6,17 +6,27 @@ def parse_argument():
     Argument definition and help message.
     '''
     parser = argparse.ArgumentParser(description='List up all stacks.')
+    parser.add_argument('-p', '--python-ip-list', dest='python_ip_list', action='store_true')
+    parser.add_argument('-i', '--ip-only', dest='ip_only', action='store_true')
     return parser.parse_args()
 
 def main():
     args = parse_argument()
 
     cfr = boto3.resource('cloudformation')
-
-    print('Name, Status, IP')
+    
+    if not args.ip_only and not args.python_ip_list:
+        print('Name, Status, IP')
+        
     for stack in cfr.stacks.all():
-        if (stack.name.startswith('msg-')):
-            print('{0}, {1}, {2}'.format(stack.name, stack.stack_status, find_public_ip(stack.outputs)))
+        if stack.name.startswith('msg-'):
+            public_ip = find_public_ip(stack.outputs)
+            if args.python_ip_list:
+                print('    \'{0}\','.format(public_ip))
+            elif args.ip_only:
+                print(format(public_ip))
+            else:
+                print('{0}, {1}, {2}'.format(stack.name, stack.stack_status, public_ip))
 
 def find_public_ip(outputs):
     public_ip = 'unknown'
