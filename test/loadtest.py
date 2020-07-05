029ee150b534b6fade97e27be527b32f85942922
Hiroshi Ohno <46858959+hiroshi-ohno@users.noreply.github.com>
2020-07-05 10:31:04 -0700

    update load test

--- test/loadtest.py
+++ test/loadtest.py
@@ -5,10 +5,10 @@ import random
 
 NUM_REQUESTS = 100
 
-REGIST_URL = 'http://name3.ninja.fish/register'
+REGIST_URL = 'http://name.ninja.fish/register'
 REGIST_TIMEOUT = 240
 
-HOSTS_URL = 'http://name3.ninja.fish/hosts'
+HOSTS_URL = 'http://name.ninja.fish/hosts'
 HOSTS_TIMEOUT = 30
 
 '''
@@ -21,25 +21,25 @@ def request_hosts():
     sleep_seconds = random.randint(1,5)
     time.sleep(sleep_seconds)
 
-    start = time.time()
+    start1 = time.time()
     with requests.Session() as session:
         adapter = requests.adapters.HTTPAdapter(max_retries=0)
         session.mount('http://', adapter)
 
     response = requests.get(HOSTS_URL, timeout=HOSTS_TIMEOUT)
     status_code1 = response.status_code
-    process1_sec = time.time() - start
+    process1_sec = time.time() - start1
 
     time.sleep(20)
 
-    start = time.time()
+    start2 = time.time()
     with requests.Session() as session:
         adapter = requests.adapters.HTTPAdapter(max_retries=0)
         session.mount('http://', adapter)
 
     response = requests.get(HOSTS_URL, timeout=HOSTS_TIMEOUT)
     status_code2 = response.status_code
-    process2_sec = time.time() - start
+    process2_sec = time.time() - start2
 
     return '{0}, {1:.2f}, process1_sec, {2}, {3:.2f}, process2_sec, {4}, init_sleep'.format(
         status_code1, process1_sec, status_code2, process2_sec, sleep_seconds)
@@ -57,7 +57,7 @@ def request_registration():
 
         payload = {
             'ipaddress': '10.1.1.1',
-            'hostname': 'load-test-{0:05}'.format(random.randint(0,100000))
+            'hostname': 'z-load-test-{0:05}'.format(random.randint(0,100000))
         }
         response = session.post(REGIST_URL, data=payload, timeout=REGIST_TIMEOUT)
         process_sec = time.time() - start
@@ -75,7 +75,7 @@ def main():
     result_list = []
     random.seed()
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_REQUESTS*2) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_REQUESTS*3) as executor:
         future_requests = []
         for i in range(0, NUM_REQUESTS):
             future_requests.append(executor.submit(request_registration))
